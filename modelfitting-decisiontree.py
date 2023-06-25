3c03d32a9eefa1f3ffa435de00d704092d263a86
amcareem <amcareem@gmail.com>
2023-06-25 15:20:14 +0530

    init

--- modelfitting-decisiontree.py
+++ modelfitting-decisiontree.py
@@ -1,5 +1,6 @@
 import pandas as pd
 from pandas import read_csv
+from sklearn.impute import SimpleImputer
 
 # Declaring header names
 winsconsin_headers = ['sample_code', 'c thickness', 'uni_cell_size', 'uni_cell_shape', 'marg_adhesion', 'epi_cell_size', 'nuclei', 'bland_chromatin', 'normal_nucleoli', 'mitoses', 'tumor_class']
@@ -16,6 +17,12 @@ print(wins_data.head(5))  # Print the first 5 rows
 print("\nLast 5 data:")
 print(wins_data.tail(5))  # Print the last 5 rows
 
+# Replace '?' with NaN
+wins_data['nuclei'].replace('?', float('nan'), inplace=True)
+
+# Convert all variables to float
+wins_data = wins_data.astype(float)
+
 # Checking for missing data
 win_data_numeric = wins_data.copy()  # Create a copy of the data
 win_data_numeric['nuclei'] = pd.to_numeric(win_data_numeric['nuclei'], errors='coerce')  # Convert 'nuclei' column to numeric
@@ -25,6 +32,10 @@ print('\nThese are the missing data:\n', win_empty_data)
 
 print("\nThese are the data types:")
 print(win_data_numeric.dtypes)  # Print the data types
+
+# Comment out the replacement process using SimpleImputer
+# imputer = SimpleImputer(strategy='mean')
+# win_data_numeric['nuclei'] = imputer.fit_transform(win_data_numeric[['nuclei']])
 
 # Overview of dataset statistics for all columns
 print("\nDataset Statistics:")
